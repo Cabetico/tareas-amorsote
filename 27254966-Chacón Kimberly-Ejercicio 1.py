d635562532bf9250155f227942ba776170060055
Cabetico <karz09@gmail.com>
2025-11-29 18:32:54 -0400

    primer taller intro soluciones

---
+++ 27254966-Chacón Kimberly-Ejercicio 1.py
@@ -0,0 +1,13 @@
+"""
+1- Crea un programa que solicite 3 números.
+   Los números pueden ser enteros o reales
+   y calcule la multplicación de los 3 números
+   y lo muestre por pantalla  (10 pts)
+"""
+valor1 = float(input("Ingrese el primer número: "))
+valor2 = float(input("Ingrese el segundo número: "))
+valor3 = float(input("Ingrese el tercer número: "))
+
+resultado = valor1 * valor2 * valor3
+
+print("El resultado de la multiplicación es:", resultado)
---
+++ 27254966-Chacón Kimberly-Ejercicio 2.py
@@ -0,0 +1,17 @@
+"""
+2-  Crea un programa que pida su nombre y su (primer) apellido al usuario, 
+	A partir de estos datos, deberá crear una cadena que contenga el apellido, 
+	una coma, 
+	un espacio y el nombre, y deberá mostrarla en pantalla. 
+	
+	Por ejemplo, 
+	
+	Si el nombre es Nacho y el apellido es Cabanes 
+	la cadena resultante será Cabanes, Nacho (10 pts)
+"""
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su primer apellido: ")
+
+resultado = apellido + ", " + nombre
+
+print("Welcome:", resultado)
